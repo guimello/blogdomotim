b9d640fbe111c21cfe5af99b44debc73675825d7
guimello <guilhermesilvamello@gmail.com>
2011-02-04 21:26:50 -0200

    More initial commit.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Blogdomotim
   class Application < Rails::Application
+    # Added by the Rails 3 jQuery Template
+    # http://github.com/lleger/Rails-3-jQuery, written by Logan Leger
+    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+    config.action_view.javascript_expansions[:cdn]      = %w(https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js rails)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -24,14 +29,15 @@ module Blogdomotim
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Brasilia'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :pt
+    config.i18n.default_locale = :'pt-BR'
 
     config.generators do |g|
       g.template_engine :haml
+      g.test_framework  :rspec
     end
 
     # JavaScript files you want as :defaults (application.js is always included).
@@ -41,6 +47,6 @@ module Blogdomotim
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Blogdomotim::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => '0.0.0.0:3000' }
 end
