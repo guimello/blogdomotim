c3d198a673d2364e7c221aa7fd4c1858f3b3d11e
guimello <guilhermesilvamello@gmail.com>
2011-02-10 21:36:33 -0200

    Preparing user registration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,25 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
-                  :name,  :bithday
+                  :name,  :birthday
+
+  validates :name, :presence => true, :allow_blank => true, :length => { :minimum => 1, :maximum => 255 }
+  validates :birthday, :presence => true, :if => proc { |user| user.birthday.blank? && user.birthday_before_type_cast.blank? }
+  validate  :birthday_format
+
+  protected
+
+  def birthday_format
+    db_date_regex = %r(^(\d{1,4})-(\d{1,2})-(\d{1,2})$)
+
+    if self.birthday_before_type_cast.present?
+      if self.birthday_before_type_cast.to_local_datetime
+        self.birthday = self.birthday_before_type_cast.to_local_datetime.to_date
+      elsif self.birthday_before_type_cast =~ db_date_regex
+        self.birthday = Time.zone.parse(self.birthday_before_type_cast)
+      else
+        self.errors.add :birthday, I18n.t(:birthday, :scope => [:user, :invalid])
+      end
+    end
+  end
 end
