22c8817653b56f98d4cd7f83782ee8666091e7d2
guimello <guilhermesilvamello@gmail.com>
2011-02-11 16:22:41 -0200

    jQuery ui css + datepicker on user sign up.

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Blogdomotim
   class Application < Rails::Application
     # Added by the Rails 3 jQuery Template
     # http://github.com/lleger/Rails-3-jQuery, written by Logan Leger
-    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery rails jquery-ui.min jquery.ui.datepicker-pt-BR application)
     config.action_view.javascript_expansions[:cdn]      = %w(https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js rails)
 
     # Settings in config/environments/* take precedence over those specified here.
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+  $('.datepicker').each(function(){
+    var options = { minDate:      $(this).attr('data-mindate'),
+                    maxDate:      $(this).attr('data-maxdate'),
+                    showAnim:     $(this).attr('data-showanim'),
+                    changeMonth:  $(this).attr('data-changemonth'),
+                    changeYear:   $(this).attr('data-changeyear')};
+
+    var defaults = jQuery.extend({
+                                  showAnim: 'drop',
+                                  changeMonth: false,
+                                  changeYear: false,
+                                  yearRange: 'c-100:c' // today - 100 years
+                                  }, options)
+
+    $(this).datepicker(defaults);
+  });
+});
