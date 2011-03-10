8c59ed19498a48bc96df8408c82dca69beeeef96
guimello <guilhermesilvamello@gmail.com>
2011-03-10 20:47:38 -0300

    Post controller not using inherited resources anymore.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,23 @@
-class PostsController < InheritedResources::Base
-  actions :show, :index
+class PostsController < ApplicationController
   respond_to :html
+  respond_to :xml, :only => :index
+
+  authorize_resource
+
+  before_filter :find_current, :only => :show
+
+  def index
+    @posts = Post.all
+    respond_with @posts
+  end
+
+  def show
+    respond_with @post
+  end
+
+  protected
+
+  def find_current
+    @post = Post.find(params[:id])
+  end
 end
